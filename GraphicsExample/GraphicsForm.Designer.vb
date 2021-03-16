7d17ac745f33f755f8726cf1ba5cfc430787e749
Tim Rossiter <rosstimo@isu.edu>
2021-03-16 08:52:17 -0600

    Add project files.

---
+++ GraphicsExample/GraphicsForm.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GraphicsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DisplayPictureBox
+        '
+        Me.DisplayPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(26, 25)
+        Me.DisplayPictureBox.Name = "DisplayPictureBox"
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(733, 374)
+        Me.DisplayPictureBox.TabIndex = 0
+        Me.DisplayPictureBox.TabStop = False
+        '
+        'GraphicsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplayPictureBox)
+        Me.Name = "GraphicsForm"
+        Me.Text = "Griphics"
+        CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DisplayPictureBox As PictureBox
+End Class
---
+++ GraphicsExample/GraphicsForm.vb
@@ -0,0 +1,32 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class GraphicsForm
+
+
+    Sub DrawLine()
+        'constructor for the graphics object
+        Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+        'actually draw the line
+        g.DrawLine(pen, 0, 0, 100, 100)
+        'free up resources
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub DrawCircle()
+        'constructor for the graphics object
+        Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
+        Dim pen As New Pen(Color.Red)
+        g.DrawEllipse(pen, 0, 0, 100, 100)
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Private Sub GraphicsForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+        DrawCircle()
+    End Sub
+
+End Class
