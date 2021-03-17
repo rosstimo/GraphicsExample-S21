4cf6093fd24b9f6a294df6719a671b9c68ce52f7
rosstimo <rosstimo@isu.edu>
2021-03-17 08:51:34 -0600

    add MouseDraw

--- GraphicsExample/GraphicsForm.vb
+++ GraphicsExample/GraphicsForm.vb
@@ -4,6 +4,7 @@ Option Strict On
 Public Class GraphicsForm
 
 
+
     Sub DrawLine()
         'constructor for the graphics object
         Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
@@ -27,6 +28,26 @@ Public Class GraphicsForm
     Private Sub GraphicsForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         DrawLine()
         DrawCircle()
+    End Sub
+
+    Private Sub DisplayPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseMove
+        Static oldX As Integer
+        Static oldY As Integer
+        Me.Text = $"({e.X},{e.Y})"
+        MouseDraw(oldX, oldY, e.X, e.Y)
+        oldX = e.X
+        oldY = e.Y
+    End Sub
+
+    Sub MouseDraw(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        'constructor for the graphics object
+        Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+        'actually draw the line
+        g.DrawLine(pen, x1, y1, x2, y2)
+        'free up resources
+        pen.Dispose()
+        g.Dispose()
     End Sub
 
 End Class
