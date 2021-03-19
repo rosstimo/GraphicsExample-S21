bbe3783163a166ad94a83305e59a308b865c1595
rosstimo <rosstimo@isu.edu>
2021-03-19 08:35:52 -0600

    add LastPoint function

--- GraphicsExample/GraphicsForm.vb
+++ GraphicsExample/GraphicsForm.vb
@@ -3,49 +3,23 @@ Option Strict On
 
 Public Class GraphicsForm
 
-    Sub DrawLine()
-        'constructor for the graphics object
-        Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
-        Dim pen As New Pen(Color.Black)
-        'actually draw the line
-        g.DrawLine(pen, 0, 0, 100, 100)
-        'free up resources
-        pen.Dispose()
-        g.Dispose()
-    End Sub
-
-    Sub DrawCircle()
-        'constructor for the graphics object
-        Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
-        Dim pen As New Pen(Color.Red)
-        g.DrawEllipse(pen, 0, 0, 100, 100)
-        pen.Dispose()
-        g.Dispose()
-    End Sub
-
     Private Sub GraphicsForm_Click(sender As Object, e As EventArgs) Handles Me.Click
-        DrawLine()
-        DrawCircle()
+        'DrawLine()
+        'DrawCircle()
+        'LastPoint(100, 100)
+        'LastPoint(10, 10)
+        'LastPoint(3, 77)
+        'Me.Text = CStr(LastPoint(,, False).Y)
+        'Me.Text = CStr(LastPoint().Y)
+        'Console.WriteLine()
+
+
     End Sub
 
-    'Private Sub DisplayPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseMove
-    '    Static oldX As Integer
-    '    Static oldY As Integer
-    '    If oldX <> 0 And oldY <> 0 Then
-    '        'MouseDraw(oldX, oldY, e.X, e.Y)
-    '    End If
-    '    'Me.Text = $"({e.X},{e.Y})"
-    '    oldX = e.X
-    '    oldY = e.Y
-    'End Sub
-
-    Sub MouseDraw(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer, penColor As Color)
-        'constructor for the graphics object
+    Sub DrawLineSegment(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer, penColor As Color)
         Dim g As Graphics = DisplayPictureBox.CreateGraphics 'Me.CreateGraphics
-        'Dim penColor As Color
-        'penColor = Color.Red
         Dim pen As New Pen(penColor)
-        'actually draw the line
+        'Draw line segment
         g.DrawLine(pen, x1, y1, x2, y2)
         'free up resources
         pen.Dispose()
@@ -53,14 +27,19 @@ Public Class GraphicsForm
     End Sub
 
     Private Sub DisplayPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseDown, DisplayPictureBox.MouseMove
-        Static oldX As Integer
-        Static oldY As Integer
+        'Static oldX As Integer
+        'Static oldY As Integer
+
+        'Dim myPoint As Point
+        'myPoint.X = 0
+        'myPoint.Y = 0
+
         Static penColor As Color
 
         Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {penColor.ToString}"
         Select Case e.Button.ToString
             Case "Left"
-                MouseDraw(oldX, oldY, e.X, e.Y, penColor)
+                DrawLineSegment(LastPoint(,, False).X, LastPoint(,, False).Y, e.X, e.Y, penColor)
             Case "Right"
 
             Case "Middle"
@@ -71,8 +50,22 @@ Public Class GraphicsForm
             Case Else
                 MsgBox("It Got Weird")
         End Select
-        oldX = e.X
-        oldY = e.Y
+        'oldX = e.X
+        'oldY = e.Y
+        LastPoint(e.X, e.Y)
+
     End Sub
+
+    Function LastPoint(Optional x As Integer = 0, Optional y As Integer = 0, Optional update As Boolean = True) As Point
+        Static _lastPoint As Point
+
+        If update = True Then
+            _lastPoint.X = x
+            _lastPoint.Y = y
+        End If
+
+        Return _lastPoint
+    End Function
+
 
 End Class
