ded2f762f8b68cc3871dc95e2748a3c2745ea53d
rosstimo <rosstimo@isu.edu>
2021-03-24 08:53:08 -0600

    add scale factor and draw waves

--- GraphicsExample/GraphicsForm.Designer.vb
+++ GraphicsExample/GraphicsForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class GraphicsForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class GraphicsForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.DisplayPictureBox = New System.Windows.Forms.PictureBox()
@@ -33,9 +33,19 @@ Partial Class GraphicsForm
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.GroupBox = New System.Windows.Forms.GroupBox()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.PointStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MouseButtonStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.PenColorStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.yScaleStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.xScaleStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.dxStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.dyStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.RotationStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
         CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ContextMenuStrip.SuspendLayout()
         Me.GroupBox.SuspendLayout()
+        Me.StatusStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'DisplayPictureBox
@@ -45,10 +55,9 @@ Partial Class GraphicsForm
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.DisplayPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.DisplayPictureBox.ContextMenuStrip = Me.ContextMenuStrip
-        Me.DisplayPictureBox.Location = New System.Drawing.Point(8, 8)
-        Me.DisplayPictureBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.DisplayPictureBox.Location = New System.Drawing.Point(12, 12)
         Me.DisplayPictureBox.Name = "DisplayPictureBox"
-        Me.DisplayPictureBox.Size = New System.Drawing.Size(488, 198)
+        Me.DisplayPictureBox.Size = New System.Drawing.Size(732, 341)
         Me.DisplayPictureBox.TabIndex = 0
         Me.DisplayPictureBox.TabStop = False
         '
@@ -57,53 +66,50 @@ Partial Class GraphicsForm
         Me.ContextMenuStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
         Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.WidthToolStripMenuItem, Me.ColorToolStripMenuItem})
         Me.ContextMenuStrip.Name = "ContextMenuStrip"
-        Me.ContextMenuStrip.Size = New System.Drawing.Size(107, 48)
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(133, 68)
         '
         'WidthToolStripMenuItem
         '
         Me.WidthToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PenWidthToolStripTextBox})
         Me.WidthToolStripMenuItem.Name = "WidthToolStripMenuItem"
-        Me.WidthToolStripMenuItem.Size = New System.Drawing.Size(106, 22)
+        Me.WidthToolStripMenuItem.Size = New System.Drawing.Size(132, 32)
         Me.WidthToolStripMenuItem.Text = "&Width"
         '
         'PenWidthToolStripTextBox
         '
         Me.PenWidthToolStripTextBox.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.PenWidthToolStripTextBox.Name = "PenWidthToolStripTextBox"
-        Me.PenWidthToolStripTextBox.Size = New System.Drawing.Size(100, 23)
+        Me.PenWidthToolStripTextBox.Size = New System.Drawing.Size(100, 31)
         '
         'ColorToolStripMenuItem
         '
         Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
-        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(106, 22)
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(132, 32)
         Me.ColorToolStripMenuItem.Text = "&Color"
         '
         'DrawButton
         '
-        Me.DrawButton.Location = New System.Drawing.Point(180, 8)
-        Me.DrawButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.DrawButton.Location = New System.Drawing.Point(270, 12)
         Me.DrawButton.Name = "DrawButton"
-        Me.DrawButton.Size = New System.Drawing.Size(99, 53)
+        Me.DrawButton.Size = New System.Drawing.Size(148, 82)
         Me.DrawButton.TabIndex = 1
         Me.DrawButton.Text = "&Draw"
         Me.DrawButton.UseVisualStyleBackColor = True
         '
         'ClearButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(283, 8)
-        Me.ClearButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.ClearButton.Location = New System.Drawing.Point(424, 12)
         Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(99, 53)
+        Me.ClearButton.Size = New System.Drawing.Size(148, 82)
         Me.ClearButton.TabIndex = 2
         Me.ClearButton.Text = "&Clear"
         Me.ClearButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(385, 8)
-        Me.ExitButton.Margin = New System.Windows.Forms.Padding(2)
+        Me.ExitButton.Location = New System.Drawing.Point(578, 12)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(99, 53)
+        Me.ExitButton.Size = New System.Drawing.Size(148, 82)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
@@ -114,29 +120,88 @@ Partial Class GraphicsForm
         Me.GroupBox.Controls.Add(Me.DrawButton)
         Me.GroupBox.Controls.Add(Me.ExitButton)
         Me.GroupBox.Controls.Add(Me.ClearButton)
-        Me.GroupBox.Location = New System.Drawing.Point(8, 210)
-        Me.GroupBox.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox.Location = New System.Drawing.Point(12, 359)
         Me.GroupBox.Name = "GroupBox"
-        Me.GroupBox.Padding = New System.Windows.Forms.Padding(2)
-        Me.GroupBox.Size = New System.Drawing.Size(488, 65)
+        Me.GroupBox.Size = New System.Drawing.Size(732, 100)
         Me.GroupBox.TabIndex = 4
         Me.GroupBox.TabStop = False
         '
+        'StatusStrip
+        '
+        Me.StatusStrip.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PointStatusLabel, Me.MouseButtonStatusLabel, Me.PenColorStatusLabel, Me.xScaleStatusLabel, Me.yScaleStatusLabel, Me.dxStatusLabel, Me.dyStatusLabel, Me.RotationStatusLabel})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 495)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(772, 32)
+        Me.StatusStrip.TabIndex = 5
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'PointStatusLabel
+        '
+        Me.PointStatusLabel.Name = "PointStatusLabel"
+        Me.PointStatusLabel.Size = New System.Drawing.Size(47, 25)
+        Me.PointStatusLabel.Text = "(X,Y)"
+        '
+        'MouseButtonStatusLabel
+        '
+        Me.MouseButtonStatusLabel.Name = "MouseButtonStatusLabel"
+        Me.MouseButtonStatusLabel.Size = New System.Drawing.Size(66, 25)
+        Me.MouseButtonStatusLabel.Text = "Mouse"
+        '
+        'PenColorStatusLabel
+        '
+        Me.PenColorStatusLabel.Name = "PenColorStatusLabel"
+        Me.PenColorStatusLabel.Size = New System.Drawing.Size(55, 25)
+        Me.PenColorStatusLabel.Text = "Color"
+        '
+        'yScaleStatusLabel
+        '
+        Me.yScaleStatusLabel.Name = "yScaleStatusLabel"
+        Me.yScaleStatusLabel.Size = New System.Drawing.Size(61, 25)
+        Me.yScaleStatusLabel.Text = "yScale"
+        '
+        'xScaleStatusLabel
+        '
+        Me.xScaleStatusLabel.Name = "xScaleStatusLabel"
+        Me.xScaleStatusLabel.Size = New System.Drawing.Size(60, 25)
+        Me.xScaleStatusLabel.Text = "xScale"
+        '
+        'dxStatusLabel
+        '
+        Me.dxStatusLabel.Name = "dxStatusLabel"
+        Me.dxStatusLabel.Size = New System.Drawing.Size(31, 25)
+        Me.dxStatusLabel.Text = "dx"
+        '
+        'dyStatusLabel
+        '
+        Me.dyStatusLabel.Name = "dyStatusLabel"
+        Me.dyStatusLabel.Size = New System.Drawing.Size(32, 25)
+        Me.dyStatusLabel.Text = "dy"
+        '
+        'RotationStatusLabel
+        '
+        Me.RotationStatusLabel.Name = "RotationStatusLabel"
+        Me.RotationStatusLabel.Size = New System.Drawing.Size(79, 25)
+        Me.RotationStatusLabel.Text = "Rotation"
+        '
         'GraphicsForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(515, 283)
+        Me.ClientSize = New System.Drawing.Size(772, 527)
+        Me.Controls.Add(Me.StatusStrip)
         Me.Controls.Add(Me.GroupBox)
         Me.Controls.Add(Me.DisplayPictureBox)
-        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "GraphicsForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Griphics"
         CType(Me.DisplayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ContextMenuStrip.ResumeLayout(False)
         Me.GroupBox.ResumeLayout(False)
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -150,4 +215,13 @@ Partial Class GraphicsForm
     Friend WithEvents WidthToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents PenWidthToolStripTextBox As ToolStripTextBox
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents PointStatusLabel As ToolStripStatusLabel
+    Friend WithEvents MouseButtonStatusLabel As ToolStripStatusLabel
+    Friend WithEvents PenColorStatusLabel As ToolStripStatusLabel
+    Friend WithEvents xScaleStatusLabel As ToolStripStatusLabel
+    Friend WithEvents yScaleStatusLabel As ToolStripStatusLabel
+    Friend WithEvents dxStatusLabel As ToolStripStatusLabel
+    Friend WithEvents dyStatusLabel As ToolStripStatusLabel
+    Friend WithEvents RotationStatusLabel As ToolStripStatusLabel
 End Class
--- GraphicsExample/GraphicsForm.vb
+++ GraphicsExample/GraphicsForm.vb
@@ -14,32 +14,9 @@ Public Class GraphicsForm
     'point tracking
     Dim lastPoint As Point
 
+
     Sub DrawLineSegment(startPoint As Point, endPoint As Point)
         g.DrawLine(Me.mainPen, startPoint, endPoint)
-    End Sub
-
-    Private Sub DisplayPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseDown, DisplayPictureBox.MouseMove
-
-        'action selection based on mouse button
-        Select Case e.Button.ToString
-            Case "Left"
-                DrawLineSegment(lastPoint, e.Location)
-            Case "Right"
-                'Not used due to context menu implementation
-            Case "Middle"
-                ColorDialog.ShowDialog()
-                Me.mainPen.Color = ColorDialog.Color
-            Case "None"
-                'TODO
-        End Select
-
-        'Update info
-        Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {Me.mainPen.Color.ToString}"
-
-        'update last known point
-        Me.lastPoint.X = e.X
-        Me.lastPoint.Y = e.Y
-
     End Sub
 
     'needs testing  with global
@@ -71,23 +48,125 @@ Public Class GraphicsForm
         Me.g.RotateTransform(rotation)
     End Sub
 
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Me.Close()
+    ''' <summary>
+    ''' Mouse Events. Track Mouse Move Coordinates, Draw on Left Button,
+    ''' Choose Pen color on middle mouse
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub DisplayPictureBox_MouseDown(sender As Object, e As MouseEventArgs) Handles DisplayPictureBox.MouseDown, DisplayPictureBox.MouseMove
+
+
+
+
+        'action selection based on mouse button
+        Select Case e.Button.ToString
+            Case "Left"
+                DrawLineSegment(lastPoint, e.Location)
+            Case "Right"
+                'Not used due to context menu implementation
+            Case "Middle"
+                ColorDialog.ShowDialog()
+                Me.mainPen.Color = ColorDialog.Color
+            Case "None"
+                'TODO
+        End Select
+
+        'Update info
+        Me.Text = $"({e.X},{e.Y}) Button: {e.Button.ToString} Color: {Me.mainPen.Color.ToString}"
+        PointStatusLabel.Text = $"({e.X},{e.Y})"
+        MouseButtonStatusLabel.Text = $"{e.Button.ToString}"
+        PenColorStatusLabel.Text = $"{Me.mainPen.Color.ToString}"
+
+        'update last known point
+        Me.lastPoint.X = e.X
+        Me.lastPoint.Y = e.Y
+
     End Sub
 
-    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+    Sub DrawWave()
+        Dim wave As Graphics = DisplayPictureBox.CreateGraphics
+        Dim _start As Point
+        Dim _end As Point
+
+        'offset amount
+        Dim yOffset As Single = CSng(DisplayPictureBox.Height / 2)
+        wave.TranslateTransform(0, yOffset)
+
+        'Scale 
+        'This will be the width of the scaled graphics object 
+        Dim xMax As Single = 360
+        Dim xScale As Single = CSng(DisplayPictureBox.Width / xMax)
+        'This will be the height of the scaled graphics object 
+        Dim yMax As Single = 100
+        'half the height in pixels is to 100 units
+        '100 up, 100 down
+        Dim yScale As Single = CSng(((DisplayPictureBox.Height) / 2) / yMax)
+        wave.ScaleTransform(xScale, yScale) 'apply scale
+
+        mainPen.Color = Color.Red
+
+        'plot Sin
+        _start.X = 0
+        _start.Y = 0
+        'Vi = Vp * sin(360 * f * t + theta) + DC
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            _end.Y = CInt(yMax * Sin((PI / 180) * _end.X)) * -1
+            wave.DrawLine(mainPen, _start, _end)
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+
+        mainPen.Color = Color.Green
+        'plot Cos
+        _start.X = 0
+        _start.Y = 0
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            _end.Y = CInt(yMax * Cos((PI / 180) * _end.X)) * -1
+            wave.DrawLine(mainPen, _start, _end)
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+        mainPen.Color = Color.Blue
+        'plot Tan
+        _start.X = 0
+        _start.Y = 0
+        For i = 0 To CInt(xMax)
+            _end.X = i
+            Try
+                _end.Y = CInt(yMax * Tan((PI / 180) * _end.X)) * -1
+            Catch
+
+            End Try
+
+            wave.DrawLine(mainPen, _start, _end)
+
+            _start.X = _end.X
+            _start.Y = _end.Y
+        Next
+
+        wave.Dispose()
+    End Sub
+
+    ''' <summary>
+    ''' Clear Event: Start shake sound, shake form, and refresh picture box
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub ClearEvent(sender As Object, e As EventArgs) Handles ClearButton.Click
         'TODO play shake sound
         ShakeSound()
         Shake()
         DisplayPictureBox.Refresh()
     End Sub
-
     Sub ShakeSound()
         'https://freesound.org/
         'play shake sound from project resources
         My.Computer.Audio.Play(My.Resources.shaker, AudioPlayMode.Background)
     End Sub
-
     Sub Shake()
         Dim moveAmount = 50
 
@@ -100,39 +179,29 @@ Public Class GraphicsForm
 
     End Sub
 
-    Private Sub WidthToolStripTextBox() Handles PenWidthToolStripTextBox.LostFocus
+    'Update pen width
+    Private Sub WidthToolStripTextBox(sender As Object, e As EventArgs) Handles PenWidthToolStripTextBox.LostFocus
         Try
             Me.mainPen.Width() = CSng(PenWidthToolStripTextBox.Text)
         Catch ex As Exception
 
         End Try
     End Sub
-
+    'Update pen color
     Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
         ColorDialog.ShowDialog()
         Me.mainPen.Color = ColorDialog.Color
     End Sub
-
-    Sub DrawWave()
-        Dim _start As Point
-        Dim _end As Point
-
-        'offset amount
-        Dim yOffset As Single = CSng(DisplayPictureBox.Height / 2)
-        Me.g.TranslateTransform(0, yOffset)
-
-        _end.X = 100
-        _end.Y = 100
-
-        DrawLineSegment(_start, _end)
-
-    End Sub
-
+    'Button/Menu Events
     Private Sub DrawButton_Click(sender As Object, e As EventArgs) Handles DrawButton.Click
         DrawWave()
     End Sub
-
+    'Start Up/Shut Down program
     Private Sub GraphicsForm_Load(sender As Object, e As EventArgs) Handles Me.Load
         Me.g = Me.DisplayPictureBox.CreateGraphics
     End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
 End Class
